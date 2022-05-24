dd264de0f0b752f481627cf43ac77f9dd8a2b759
Yangjinho <yangjinho826@forelink.co.kr>
2022-05-24 17:05:08 +0900

    로그인, 로그아웃, 자동로그인

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,16 @@
+import re
 from django.contrib import messages
 from django.shortcuts import redirect, render
+from django.contrib.auth.views import LoginView, logout_then_login
 from .forms import SignupForm
-
+from django.contrib.auth import login as auth_login
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
         if form.is_valid():
-            form.save()
+            signed_user = form.save()
+            auth_login(request, signed_user)
             messages.success(request, "회원가입 환영합니다.")
             next_url = request.GET.get('next', '/')
             return redirect(next_url)
@@ -16,3 +19,14 @@ def signup(request):
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
+
+# login = LoginView.as_view(template_name="accounts/login_form.html")
+
+class Login(LoginView):
+    template_name="accounts/login_form.html"
+
+login = Login.as_view()
+
+def logout(request):
+    messages.success(request, '로그아웃 성공')
+    return logout_then_login(request)
