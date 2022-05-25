83b66adab74fb60fa46fb9ad6d0bb5f71d941377
Yangjinho <yangjinho826@forelink.co.kr>
2022-05-25 14:35:13 +0900

    패스워드 변경

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm, PasswordChangeForm as AuthPasswordChangeForm
+)
 from .models import User
 
 # class SignupForm(forms.ModelForm):
@@ -33,3 +35,12 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'website_url','bio', 'phone_number','gender','avatar']
+
+class PasswordChangeForm(AuthPasswordChangeForm):
+    def clean_new_password1(self):
+        old_password = self.cleaned_data["old_password"]
+        new_password1 = self.cleaned_data["new_password1"]
+        if old_password and new_password1:
+            if old_password == new_password1:
+                raise forms.ValidationError("새로운 암호는 기존 암호과 다르게 입력해주세요.")
+        return new_password1
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
     path('edit/', views.profile_edit, name='profile_edit'),
+    path('password_change/', views.password_change, name='password_change'),
     
 ]
