ec0b0f20648b41579ad478f09af8e59d14730800
Yangjinho <yangjinho826@forelink.co.kr>
2022-05-26 10:00:47 +0900

    Timeline sidebar 템플릿/스타일 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'instagram'
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
--- instagram/views.py
+++ instagram/views.py
@@ -6,6 +6,12 @@ from .models import Post
 from django.contrib.auth import get_user_model
 
 @login_required
+def index(request):
+    return render(request, "instagram/index.html",{
+        
+    })    
+
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
